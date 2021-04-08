974fa1115df27b18e04ae6438b335a3b974e83de
workzach <workzach@mbpii.fritz.box>
2021-04-09 08:53:16 +1200

    add bootstrap for ADS management server

--- hack/start-envoy.sh
+++ hack/start-envoy.sh
@@ -6,5 +6,11 @@ set -o pipefail
 # Path to Envoy
 ENVOY=${ENVOY:-/usr/local/bin/envoy}
 
-## Start Envoy with sample bootstrap config.
-${ENVOY} -c hack/bootstrap.yaml # --drain-time-s 1  # -l debug
+# are there no input args set? then run our default
+if [[ $# -eq 0 ]]; then
+  echo "using default"
+  ${ENVOY} -c  hack/bootstrap.yaml # --drain-time-s 1  # -l debug
+else #take the first arg as the yaml to run
+  echo "using bootstrap file: $1"
+  ${ENVOY} -c  $1 # --drain-time-s 1  # -l debug
+fi
